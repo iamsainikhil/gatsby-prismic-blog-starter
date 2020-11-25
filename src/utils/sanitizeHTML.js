6134b2d49c6d8abbe2a7dac0d00245bfe24a63ce
Restyled.io <commits@restyled.io>
2020-11-25 02:36:17 +0000

    Restyled by prettier

--- src/utils/sanitizeHTML.js
+++ src/utils/sanitizeHTML.js
@@ -4,6 +4,8 @@ import DOMPurify from 'dompurify'
  * sanitize HTML
  * @param {String} html
  */
-const sanitizeHTML = (html) => { return DOMPurify.sanitize(html) }
+const sanitizeHTML = (html) => {
+  return DOMPurify.sanitize(html)
+}
 
 export default sanitizeHTML
